b512917a7f9a848f2764dc303b7f6aed5740296f
Kyaw Thu <evanch98@gmail.com>
2024-10-02 16:39:24 +0800

    testing the model

---
+++ app.py
@@ -0,0 +1,5 @@
+from preprocessing.preprocessing import predict_image, ImageURL
+
+print(predict_image(ImageURL(
+    url="https://github.com/ummadiviany/Malaria-Image-Classification/blob/main/test_images/NotInfected/C1_thinF_IMG_20150604_104722_cell_60.png?raw=true"
+)))

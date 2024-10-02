1513c399c48f7575280029d27bbaa3b0633b0e84
Kyaw Thu <evanch98@gmail.com>
2024-10-02 19:17:52 +0800

    adding the first route

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+from flask import Flask, request
+
 from preprocessing.preprocessing import predict_image, ImageURL
 
-print(predict_image(ImageURL(
-    url="https://github.com/ummadiviany/Malaria-Image-Classification/blob/main/test_images/NotInfected/C1_thinF_IMG_20150604_104722_cell_60.png?raw=true"
-)))
+app = Flask(__name__)
+
+@app.get("/malaria")
+def get_prediction():
+    request_data = request.get_json()
+    data = predict_image(ImageURL(url=request_data["url"]))
+    return data, 200

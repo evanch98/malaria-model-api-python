f0cd40b77cc759f3a8fa53e10fec08d7814181b4
Kyaw Thu <evanch98@gmail.com>
2024-10-02 19:21:03 +0800

    adding configuration

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ from preprocessing.preprocessing import predict_image, ImageURL
 
 app = Flask(__name__)
 
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Malaria Diagnosis API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+
+
 @app.get("/malaria")
 def get_prediction():
     request_data = request.get_json()

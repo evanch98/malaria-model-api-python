a1ad86e93f693ea9854d7b57ae38c928aa6f8f12
Kyaw Thu <evanch98@gmail.com>
2024-10-02 21:34:08 +0800

    updating the app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
+from flask_smorest import Api
 
-from preprocessing.preprocessing import predict_image, ImageURL
+from resources.malaria import blp as MalariaBlueprint
 
 app = Flask(__name__)
 
@@ -12,9 +13,6 @@ app.config["OPENAPI_URL_PREFIX"] = "/"
 app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
+api = Api(app)
 
-@app.get("/malaria")
-def get_prediction():
-    request_data = request.get_json()
-    data = predict_image(ImageURL(url=request_data["url"]))
-    return data, 200
+api.register_blueprint(MalariaBlueprint)
